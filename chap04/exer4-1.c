e8a2cc4e23230ee2a4f3edbf4bee260431324547
syDengWJ <1131951295@qq.com>
2019-02-18 14:22:49 +0800

    exercise 4-1

---
+++ chap04/exer4-1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int strindex(char s[], char t[]) {
+  int ns, nt;
+  int i, j, k;
+
+  ns = 0;
+  while (s[ns++] != '\0')
+    ;
+  nt = 0;
+  while (t[nt++] != '\0')
+    ;
+  // printf("%d %d %d %d\n", ns, nt, sizeof(s), sizeof(t));
+  for (i = ns-nt; s[i] != '\0'; --i) {
+    for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++) {
+      ;
+    }
+    if (k > 0 && t[k] == '\0') {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int main() {
+  char s[] = "everytime i try to fly i fall without you near me.";
+  char t[] = "try";
+  int index = strindex(s, t);
+  printf("%d\n", index);
+  return 0;
+}
